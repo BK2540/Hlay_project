54b53f676d4955a8cb6a2671a394e31656f8a5fc
unknown <boongkee2101@gmail.com>
2023-08-30 09:50:02 +0700

    commit changed

--- src/components/SingleCanvas.jsx
+++ src/components/SingleCanvas.jsx
@@ -2,17 +2,14 @@
 
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import * as THREE from 'three';
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 3;
 
 const SingleCanvas = ({ children }) => {
 
   return (
     <div className='h-full w-[350px]'>
     <Canvas
-      camera={{ fov: 45, aspect:0.6 }}
+      camera={{ fov: 55, aspect:0.6 }}
     >
         <OrbitControls/>
         <ambientLight environment="city" intensity={1} />
